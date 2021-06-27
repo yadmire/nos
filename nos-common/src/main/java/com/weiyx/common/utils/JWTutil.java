b7650561a3c08f4c72e6682cda2f74075a364633
“youralex742216537 <742216537@qq.com>
2021-06-27 22:02:25 +0800

    jwtutil01

---
+++ nos-common/src/main/java/com/weiyx/common/utils/JWTutil.java
@@ -0,0 +1,7 @@
+package com.weiyx.common.utils;
+
+public class JWTutil {
+    public static final long TOKEN_EXPIRE_TIME = 7200 * 1000;
+    private static final String ISSUER = "nos-service";
+
+}
