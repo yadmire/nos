3a1b968258ab406dded986434240e7b5748477b7
“youralex742216537 <742216537@qq.com>
2021-08-22 13:38:54 +0800

    修复调整基础架构后网关及各配置项，user服务及鉴权服务、网关服务恢复正常

--- nos-user/user-service/src/main/java/com/weiyx/nos/mapper/SysAuthorityMapper.java
+++ nos-user/user-service/src/main/java/com/weiyx/nos/mapper/SysAuthorityMapper.java
@@ -3,5 +3,8 @@ package com.weiyx.nos.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.weiyx.nos.model.SysAuthority;
 
+import java.util.List;
+
 public interface SysAuthorityMapper extends BaseMapper<SysAuthority> {
+     List<SysAuthority> searchByid(String id);
 }
